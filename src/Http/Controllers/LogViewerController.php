6672af95a84b0c915bbc9ed422e0970c24496898
Kachula Ivan <ka4ivan.dev@gmail.com>
2025-08-13 23:24:53 +0300

    add comment

--- src/Http/Controllers/LogViewerController.php
+++ src/Http/Controllers/LogViewerController.php
@@ -6,6 +6,9 @@ namespace Ka4ivan\LaravelLogger\Http\Controllers;
 
 class LogViewerController
 {
+    /**
+     * Compatibility with 1.x versions
+     */
     public function index()
     {
         return redirect()->to('log-viewer');
